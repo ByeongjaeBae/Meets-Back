5f65445f0599368e25d6c46b934f6be89e028336
배병재 <39183208+BaeByungjae@users.noreply.github.com>
2024-12-08 22:39:09 +0900

    commit

---
+++ src/main/kotlin/com/meets/meets_api/MeetsApiApplication.kt
@@ -0,0 +1,11 @@
+package com.meets.meets_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MeetsApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<MeetsApiApplication>(*args)
+}
